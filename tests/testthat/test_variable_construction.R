88b909e1c0d5c5985f27feecf61b4d4ed4cea2aa
Eunice Jun <eunice.m.jun@gmail.com>
2022-02-10 10:14:38 -0800

    Restructure tests

---
+++ tests/testthat/test_variable_construction.R
@@ -0,0 +1,87 @@
+context("Variable declaration and construction")
+library(tisaner)
+
+test_that("AbstractVariable created properly", {
+  var <- AbstractVariable("name")
+
+  expect_is(var, "AbstractVariable")
+})
+
+test_that("Unit created properly", {
+    unit <- Unit("person") 
+
+    unit <- Unit("person", cardinality=40)
+})
+
+test_that("SetUp created properly"), {
+    week <- SetUp("week", order=[1,2,3,4])
+
+    week <- SetUp("week", order=[1,2,3,4], cardinality=4)
+
+    # Throws an error
+    week <- SetUp("week", order=[1,2,3,4], cardinality=10)
+
+})
+
+test_that("Nominal measure created properly", {
+    # Create unit 
+    unit <- Unit("person")
+    
+    eye_color <- nominal(unit=unit, name="eye_color", cardinality=10)
+    
+    # Should throw error since there is no cardinality specified
+    eye_color <- nominal(unit=unit, name="eye_color")
+
+    # Verify that Has relationship constructed
+    # Verify that number of instances is set to 1 (Integer)
+})
+
+test_that("Ordinal measure created properly", {
+    # Create unit
+    unit <- Unit("person")
+
+    school_grade <- ordinal(unit=unit, name="school_grade", order=[1,2,3,4,5])
+    
+    # Throws error since @param order is not provided
+    school_grade <- ordinal(unit=unit, name="school_grade")
+
+    # Throws error since @param order and @param cardinality disagree
+    school_grade <- ordinal(unit=unit, name="school_grade", order=[1,2,3,4,5], cardinality=10)
+})
+
+test_that("Numeric measure created properly", {
+    # Create unit
+    unit <- Unit("person")
+
+    age <- numeric(unit=unit, name="age")
+
+    # Verify unit and measure relationships
+    # Verify Has relationship is constructed correctly
+})
+
+# Write test for passing Has multiple different types to number_of_instances
+test_that("Has relationship created properly", {
+    # Create unit
+    unit <- Unit("person")
+    # Create setup
+    week <- SetUp("week", order=[1,2,3,4])
+
+    age <- numeric(unit=unit, name="age")
+    # Verify that number of instaces == 1 (integer)
+
+    
+    # Integer
+    age <- numeric(unit=unit, name="age", number_of_instances=10)
+
+    # AbstractVariable
+    age <- numeric(unit=unit, name="age", number_of_instances=week)
+
+    # AtMost
+    age <- numeric(unit=unit, name="age", number_of_instances=AtMost(2))
+
+    # Per
+    age <- numeric(unit=unit, name="age", number_of_instances=Exactly(1).per(week))
+
+    # Per
+    age <- numeric(unit=unit, name="age", number_of_instances=AtMost(1).per(week))
+})
