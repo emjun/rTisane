ac9f87fb5e1d64fe7e05f0420211aa64d836ab7d
Eunice Jun <eunice.m.jun@gmail.com>
2022-02-28 18:20:24 -0800

    Update description

---
+++ R/infer_random_effects_with_explanations.R
@@ -0,0 +1,17 @@
+#' Infers random effects.
+#'
+#' This function infers random effects from data measurement relationships.
+#' @param measurement_gr Graph representing data measurement relationships.
+#' @param nests_gr Graph representing nesting relationships between variables.
+#' @param design Design
+#' @param main_effects List of candidate main effects
+#' @param interaction_effects List of candidate interaction effects
+#' @return List of random effects and their explanations
+#' @import dagitty
+#' @keywords
+#' @examples
+#' infer_interaction_effects_with_explanations()
+infer_random_effects_with_explanations <- function(measurement_gr, nests_gr, design, main_effects, interaction_effects) {
+    # TODO: START HERE
+}
+
