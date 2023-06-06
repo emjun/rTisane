f92ca0fe966e436c232d7b9e1cf3749466b81fab
Eunice Jun <eunice.m.jun@gmail.com>
2023-06-06 13:36:36 -0700

    Hardcode path to start.py

--- R/disambiguateStatisticalModel.R
+++ R/disambiguateStatisticalModel.R
@@ -2,6 +2,7 @@
 #' @import reticulate
 disambiguateStatisticallModel <- function(inputFilePath) {
     fileName = "start.py"
-    path = file.path(getwd(), "gui", fileName)
+    # path = file.path(getwd(), "gui", fileName)
+    path = "/Users/cse-loaner/Git/rTisane/gui/start.py"
     reticulate::py_run_file(path)
 }
