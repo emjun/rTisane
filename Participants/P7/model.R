06ec4d90409805940245bfb7fa0ae255c670122e
Eunice Jun <eunice.m.jun@gmail.com>
2023-06-27 13:24:34 -0700

    Add P7

--- Participants/P7/model.R
+++ Participants/P7/model.R
@@ -1,8 +1,8 @@
-install.packages('tidyverse')
-install.packages('lme4')
+#install.packages('tidyverse')
+#install.packages('lme4')
 
 library(tidyverse)
 library(lme4)
 # Replace 'PATH' with a path to your data
-data <- read.csv('PATH')
+data <- read.csv('/Users/cse-loaner/Git/rTisane/Participants/income_final.csv')
 glm(formula=Income ~ Sex+Age+Employment+Race+Education, family=gaussian(link='identity'), data=data)
