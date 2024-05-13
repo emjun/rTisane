909832236066af726f4268f7dee07d71ece15913
Eunice Jun <eunice.m.jun@gmail.com>
2024-05-13 05:39:39 -1000

    Remove files generated from running rTisane programs that are not included in the gitignore

--- model.R
+++
@@ -1,8 +0,0 @@
-install.packages('tidyverse')
-install.packages('lme4')
-
-library(tidyverse)
-library(lme4)
-# Replace 'PATH' with a path to your data
-data <- read.csv('PATH')
-glm(formula=Income ~ Education+Sex+Employment, family=gaussian(link='identity'), data=data)
